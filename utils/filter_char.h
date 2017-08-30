7a2c30d4e1dc2a913b6adf8df6a77c85b7398c7c
root <847072154@qq.com>
2017-08-30 20:30:20 +0800

    略优化代码

--- utils/filter_char.h
+++ utils/filter_char.h
@@ -6,16 +6,14 @@
 #define AXPOI_FILTER_CHAR_H
 void filter_char(char *filter_char, char filter) {
     int rel_index = 0;
-    int no_filter = 0;
     for (int i = 0; filter_char[i] != '\0'; ++i) {
 
         if (filter_char[i] != filter) {
-            no_filter++;
             filter_char[rel_index] = filter_char[i];
             rel_index++;
         }
 
     }
-    filter_char[no_filter] = '\0';
+    filter_char[rel_index] = '\0';
 }
 #endif //AXPOI_FILTER_CHAR_H
