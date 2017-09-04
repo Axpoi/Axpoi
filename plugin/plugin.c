f14affbb99831f53c2eb29ca6ab5d15c710f8550
root <847072154@qq.com>
2017-09-04 23:29:36 +0800

    emm Symbol啦..

--- plugin/plugin.c
+++ plugin/plugin.c
@@ -8,7 +8,6 @@
 #ifdef UNIX
 #include<dlfcn.h>
 #endif
-
 // 插件框架和加载器
 void plugin_load_one(char* plugin_path){
 #ifdef UNIX
@@ -18,7 +17,12 @@ void plugin_load_one(char* plugin_path){
         printf("Reason:%s\n",dlerror());
         return;
     }
-
-
+    void (*sym)() =  dlsym(plugin_handle,"on_load");
+    if(sym == NULL){
+        printf("Loading plugin's on_load function error,\n");
+        printf("Reason : %s\n",dlerror());
+        return;
+    }
+    (*sym)();
 #endif
 }
