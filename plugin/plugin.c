62a9c9102bec858e62518d53ad61d3465e061735
root <847072154@qq.com>
2017-09-07 23:12:14 +0800

    oops，一些无用的更新，最近事情太慢忙可能无法更

--- plugin/plugin.c
+++ plugin/plugin.c
@@ -4,25 +4,27 @@
 	> Mail: 
 	> Created Time: Sun 03 Sep 2017 11:00:58 AM CST
  ************************************************************************/
-// TODO Header file.
-#ifdef UNIX
-#include<dlfcn.h>
-#endif
+#include "plugin.h"
+#include <errno.h>
 // 插件框架和加载器
-void plugin_load_one(char* plugin_path){
+void* plugin_load_one(char* plugin_path){
 #ifdef UNIX
     void* plugin_handle = dlopen(path,RTLD_NOW);
     if(plugin_handle == NULL){
         printf("Load plugin error\n");
         printf("Reason:%s\n",dlerror());
-        return;
+        return plugin_handle;
     }
-    void (*sym)() =  dlsym(plugin_handle,"on_load");
-    if(sym == NULL){
-        printf("Loading plugin's on_load function error,\n");
-        printf("Reason : %s\n",dlerror());
-        return;
-    }
-    (*sym)();
 #endif
 }
+// 初始化插件加载器
+list* plugin_init(list* config){
+    list * l = NULL;
+    errno = 0;
+    list_construct(&l);
+    return l;
+}
+
+/* TODO 遍历文件夹的所有.so / .dll 动态链接库文件，并实现函数虚表内的函数
+ * 予以调用
+ */
